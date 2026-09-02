7537a939dc5dfb42f3d89ff9e2d4a6d4eedc0de8
lain <chiyomiziwari@gmail.com>
2026-09-02 12:40:29 +0700

    Add order DTOs and mapping profiles; clean up csproj
    Added OrderDetailResponse and OrderResponse DTOs for API responses. Introduced AutoMapper profiles for mapping Order and OrderDetail entities to DTOs, including custom property mappings. Removed unused <Folder> entry from Restaurant.Contract.csproj.

---
+++ src/Shared/Restaurant.Contract/DTOs/Sale/OrderDetails/OrderDetailResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant.Contract.DTOs.Sale.OrderDetails
+{
+    public class OrderDetailResponse
+    {
+        public string ProductName { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
---
+++ src/Shared/Restaurant.Contract/DTOs/Sale/Orders/OrderResponse.cs
@@ -0,0 +1,26 @@
+﻿using Restaurant.Contract.DTOs.Sale.OrderDetails;
+using Restaurant.Domain.Enums;
+
+namespace Restaurant.Contract.DTOs.Sale.Orders
+{
+    public class OrderResponse
+    {
+        public string OrderCode { get; set; } = null!;
+
+        public string CustomerCode { get; set; } = null!;
+        public string EmployeeCode { get; set; } = null!;
+        public string BranchCode { get; set; } = null!;
+
+        public OrderStatus Status { get; set; }
+        public OrderType Type { get; set; }
+
+        public decimal DiscountAmount { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal DeliveryFee { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string? Note { get; set; }
+
+        public IEnumerable<OrderDetailResponse> OrderDetails { get; set; } = [];
+    }
+}
