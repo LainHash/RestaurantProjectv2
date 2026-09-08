f8b4b012fdd64bca5e5144d318fafec2e530983e
lain <chiyomiziwari@gmail.com>
2026-09-08 12:15:34 +0700

    Remove GetByName endpoints from controllers
    Removed the GetByName endpoints from BrandsController and ProductCategoriesController, including related routes, action methods, and query imports. Controllers now only support retrieval by ID.

--- src/Restaurant.API/Controllers/Catalog/BrandsController.cs
+++ src/Restaurant.API/Controllers/Catalog/BrandsController.cs
@@ -8,7 +8,6 @@ using Restaurant.Application.Features.Catalog.Brands.Commands.Restore;
 using Restaurant.Application.Features.Catalog.Brands.Commands.Update;
 using Restaurant.Application.Features.Catalog.Brands.Queries.GetAll;
 using Restaurant.Application.Features.Catalog.Brands.Queries.GetById;
-using Restaurant.Application.Features.Catalog.Brands.Queries.GetByName;
 using Restaurant.Contract.DTOs.Catalog.Brands;
 
 namespace Restaurant.API.Controllers.Catalog
@@ -36,17 +35,6 @@ namespace Restaurant.API.Controllers.Catalog
             CancellationToken cancellationToken)
         {
             var query = new GetBrandByIdQuery(id);
-            var result = await _mediator.Send(query, cancellationToken);
-            return this.ToActionResult(result);
-        }
-
-        [AllowAnonymous]
-        [HttpGet("by-name/{name}")]
-        public async Task<IActionResult> GetByName(
-            [FromRoute] string name,
-            CancellationToken cancellationToken)
-        {
-            var query = new GetBrandByNameQuery(name);
             var result = await _mediator.Send(query, cancellationToken);
             return this.ToActionResult(result);
         }
